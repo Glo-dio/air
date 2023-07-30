2acf4334bc8e8564ac5e9c2b230e6c10b90e0a99
Glodi home <glodi@glodi-ThinkPad-P14s-Gen-3.(none)>
2023-07-30 19:19:33 +0200

    js = 2/14

--- air01.js
+++ air01.js
@@ -7,19 +7,20 @@ function ma_fonction(str, string_séparateur)
 	let tmp = []
 	let i = 0;
 	
-	while (str.charCodeAt(i) == 32 || str.charCodeAt(i) == 9 || str.charCodeAt(i) == 10)
+	while (str.charCodeAt(i) == string_séparateur.charCodeAt(0))
 		i++;
 	
 	while(i <= str.length)
 	{
 		
-		if (((str.charCodeAt(i) == 32 || str.charCodeAt(i) == 9 || str.charCodeAt(i) == 10) 
+		if ((str.charCodeAt(i) == string_séparateur.charCodeAt() 
 			&& (str.charCodeAt(i + 1) > 64 && str.charCodeAt(i + 1) < 91
 			|| str.charCodeAt(i + 1) > 96 && str.charCodeAt(i + 1) < 123)
 			|| i == str.length))
 		{
+			// tmp.push(String(word));
 			tmp.push(String(word.join('')));
-			tableau.push(tmp);
+			tableau.push(`${tmp}`);
 			tmp = [];
 			word = [];
 		}
@@ -37,7 +38,7 @@ function is_arg_valid()
 }
 
 // Partie 1 : Gestion d'erreur
-is_nb_arg_correct = process.argv.length === 3;
+is_nb_arg_correct = process.argv.length === 4;
 
 if (is_arg_valid() === false)
 {
@@ -47,11 +48,12 @@ if (is_arg_valid() === false)
 	
 // Partie 2 : Parsing
 const arg = process.argv[2];
+const separator = process.argv[3];
 
 // Partie 3 : Résolution
-const answer = ma_fonction(arg,'empty');
+const answer = ma_fonction(arg, separator);
 
 // Partie 4 : Affichage
 for (let i in answer)
-	console.log(String(answer[i]))
+	console.log(answer[i])
 
