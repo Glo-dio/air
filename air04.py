2c7cbf1f632c7ad776922a44dec6308340d4d242
Glodi home <glodi@glodi-ThinkPad-P14s-Gen-3.(none)>
2023-08-08 13:14:02 +0200

    py = 5/14

---
+++ air04.py
@@ -0,0 +1,36 @@
+import sys
+
+# fonctions utilisées
+
+def delete_siamese_character(array):
+	i = 1
+	string = []
+	string.append(array[0])
+
+	while i < len(array) - 1:
+		while array[i - 1] == array[i]:
+			i += 1
+		string.append(array[i])
+		i += 1	
+	string = ''.join(string)
+	return string
+
+def is_arg_valid():
+  if is_nb_arg_correct == False:
+    return False
+
+# Partie 1 : Gestion d'erreur
+is_nb_arg_correct = len(sys.argv) == 2
+
+if is_arg_valid() == False:
+  print("error")
+  sys.exit()
+
+# Partie 2 : Parsing
+args = sys.argv[1]
+
+# Partie 3 : Résolution
+answer = delete_siamese_character(args)
+
+# Partie 4 : Affichage
+print(answer)
